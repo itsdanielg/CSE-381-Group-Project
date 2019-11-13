45da55e0310936f6b4bb626a756dd70219704a76
Daniel Garcia <danielg_97@yahoo.com>
2019-11-13 05:35:52 -0500

    Added better movement

--- Speedbot/Assets/Scripts/CameraController.cs
+++ Speedbot/Assets/Scripts/CameraController.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public float rotationSpeed = 1;
-    public Transform Target, Player;
-    float mouseX, mouseY;
+    public Transform target, pivot, lookAt;
+    public Vector3 offset;
+    public bool useOffset;
+    public float rotationSpeed;
+
+    private float mouseX, mouseY;
+    private float minViewAngle = -70;
+    private float maxViewAngle = 20;
 
     // Start is called before the first frame update
     void Start() {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        if (useOffset) {
+            offset = target.position - transform.position;
+        }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = null;
     }
 
     void LateUpdate() {
-        controlCamera();
-    }
 
-    void controlCamera() {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        pivot.transform.position = target.transform.position;
 
-        transform.LookAt(Target);
+        mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        //mouseY = Mathf.Clamp(mouseY, -70, 50);
 
-        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        pivot.Rotate(0, mouseX, 0);
+        pivot.Rotate(-mouseY, 0, 0);
+
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f) {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle) {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        float yAngle = pivot.eulerAngles.y;
+        float xAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(xAngle, yAngle, 0);
+        transform.position = target.position - (rotation * offset);
+        transform.LookAt(lookAt);
+
+        
+        // Player.rotation = Quaternion.Euler(0, mouseX, 0);
     }
 }
---
+++ Speedbot/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController controller;
+    public Animator animator;
+    public Transform pivot;
+    public GameObject playerModel;
+
+    public float walkSpeed;
+    public float runningSpeed;
+    public float jumpRunMultiplier;
+    public float jumpHeight;
+    public float gravity;
+    public float rotationSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 lastPlayerPosition;
+
+    private bool isIdle;
+    private bool isRunning;
+    private bool isJumping;
+
+    // Start is called before the first frame update
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Update player movement
+        updateMovement();
+        // Rotate player based on camera direction if moving
+        updateRotation();
+        // Update model animations
+        updateAnimation();
+    }
+
+    void updateMovement() {
+        // Get direction vectors
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float jumpVec = moveDirection.y;
+        moveDirection = (transform.forward * vertical) + (transform.right * horizontal);
+        // Normalize vector if there is diagnonal movement
+        if (horizontal != 0 && vertical != 0) {
+            moveDirection = moveDirection.normalized;
+        }
+        // Apply run only if player is walking
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) {
+            isIdle = false;
+            if (Input.GetKeyDown(KeyCode.LeftShift) && controller.isGrounded) {
+                isRunning = !isRunning;
+            }
+        }
+        else {
+            isIdle = true;
+            isRunning = false;
+        }
+        // Change speed if player is running
+        if (isRunning) {
+            moveDirection *= runningSpeed;
+            if (isJumping) {
+                moveDirection *= jumpRunMultiplier;
+            }
+        }
+        else {
+            moveDirection *= walkSpeed;
+        }
+        // No y velocity when player is grounded & handle jump event
+        moveDirection.y = jumpVec;
+        if (controller.isGrounded) {
+            isJumping = false;
+            moveDirection.y = 0;
+            if (Input.GetButtonDown("Jump")) {
+                isIdle = false;
+                isJumping = true;
+                moveDirection.y = jumpHeight;
+            }
+        }
+        // Apply gravity and move player based on inputs
+        moveDirection.y += gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    // Method for updating player rotation
+    void updateRotation() {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    // Method for updating model animations
+    void updateAnimation() {
+        animator.SetBool("isIdle", isIdle);
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isGrounded", controller.isGrounded);
+    }
+
+}
