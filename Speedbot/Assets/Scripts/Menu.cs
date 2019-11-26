3651376e82fce85b4f87b8750efd9d7609c22118
Daniel Garcia <danielg_97@yahoo.com>
2019-11-26 05:08:42 -0500

    Added main menu, adjusted tutorial

---
+++ Speedbot/Assets/Scripts/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject FindInActiveObjectByName(string name)
+    {
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].name == name)
+                {
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void OpenLevelMenu()
+    {
+        gameObject.SetActive(false);
+        FindInActiveObjectByName("LevelMenu").SetActive(true);
+    }
+
+    public void OpenControlMenu()
+    {
+        gameObject.SetActive(false);
+        FindInActiveObjectByName("ControlMenu").SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        gameObject.SetActive(false);
+        FindInActiveObjectByName("MainMenu").SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
